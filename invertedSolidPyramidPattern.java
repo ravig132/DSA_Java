74d709d04fef9b296d83f795d41121d9c6aa14db
Ravi <gangwarr132@gmail.com>
2026-08-02 00:39:25 +0530

    inverted solid pyramid pattern question

---
+++ invertedSolidPyramidPattern.java
@@ -0,0 +1,16 @@
+public class invertedSolidPyramidPattern {
+    public static void main (String [] args ){
+        int n = 5 ;
+
+        for (int row = 1; row<=n; row++ ){
+            for (int col = 1 ; col<=row-1; col++){
+                System.out.print("  ");
+            }
+            for (int col = 1; col<=(2*n - 2*row + 1); col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
