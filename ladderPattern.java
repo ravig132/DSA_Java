750f6a984fdf6da52ddd7baebc09479f075982af
Ravi <gangwarr132@gmail.com>
2026-08-01 17:45:08 +0530

    ladder pattern question

--- ladderPattern.java
+++ ladderPattern.java
@@ -6,30 +6,17 @@ public class ladderPattern {
 
         System.out.print("Enter the Number : ");
         int num = input.nextInt();
-
-        for (int i = 1; i <= (3*num) ; i++) {
-            for (int j = 1; j <= i ; j++) {
-                if ((i!=j)){
+        for (int i = 0; i < num ; i++) {
+            for (int j = 1; j <=num ; j++){
+                for (int l = 0; l < (num-1)*i ; l++) {
                     System.out.print("  ");
-                }else{
-                   printSquare(num);
                 }
+                for (int k = 1 ; k<= num; k++){
+                    System.out.print("* ");
+                }
+                System.out.println();
 
             }
-            System.out.println();
-        }
-
-
-
-
-    }
-
-    public static void printSquare(int num){
-        for (int l = 1; l <= num ; l++) {
-            for (int k = 1; k <=num ; k++) {
-                System.out.print("* ");
-            }
-            System.out.println();
         }
     }
 }
