b70d1fe9f4cd9794738780d48f3198c300c56a49
Ravi <gangwarr132@gmail.com>
2026-08-02 00:15:06 +0530

    solid pyramid pattern quetion improvised

--- solidPyramidPattern.java
+++ solidPyramidPattern.java
@@ -9,9 +9,6 @@ public class solidPyramidPattern {
             for (int col = 1; col<=2*row-1 ; col++){
                 System.out.print("* ");
             }
-            for (int col = 1; col<=n-row ;col++){
-                System.out.print("  ");
-            }
             System.out.println();
         }
     }
