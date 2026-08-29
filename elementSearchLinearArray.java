ab3ee07aacf62132c017c2f94c9fa1d7da994874
Ravi <gangwarr132@gmail.com>
2026-08-29 09:37:25 +0530

    linear search problem solved

--- elementSearchLinearArray.java
+++ elementSearchLinearArray.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class elementSearchLinearArray {
+    public static void main (String [] args ){
+        int [] arr = {2,4,5,8,3,1};
+        int target = 20 ;
+        if (linearSearch(arr,target)){
+            System.out.println("element found");
+        }else {
+            System.out.println("element not found");
+        }
+
+    }
+    static boolean linearSearch(int [] arr , int target ){
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i]==target){
+                return true ;
+            }
+        }
+        return false ;
+    }
+
 }
