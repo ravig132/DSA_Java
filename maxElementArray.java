48103d1684712bb6359b73beed1d0997bead68b7
Ravi <gangwarr132@gmail.com>
2026-08-29 09:38:21 +0530

    maximum element from array problem solved and readme updated

--- maxElementArray.java
+++ maxElementArray.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class maxElementArray {
+    public static void main (String[] args){
+        int [] arr = {2,4,8,60,1,14};
+        System.out.println(maxElement(arr));
+    }
+    static int maxElement(int [] arr){
+        int max = 0 ;
+
+        for (int i = 0 ; i<arr.length; i++){
+            if (arr[i]>max){
+                max = arr[i];
+            }
+        }
+
+
+        return max ;
+    }
+
 }
