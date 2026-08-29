85b3ffe453d439b453de0ef172483f772ebf0629
Ravi <gangwarr132@gmail.com>
2026-08-29 09:36:39 +0530

    Scaling the element by 10 logic solved

--- elementScalingByTenArray.java
+++ elementScalingByTenArray.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class elementScalingByTenArray {
+    public static void main(String[] args){
+        int [] arr = {2,3,5,9,12};
+        int [] newArr = elementScaling(arr);
+
+        for (int i : newArr ){
+            System.out.print(i+" ");
+        }
+
+    }
+    static int[] elementScaling(int[] arr){
+        for (int i = 0; i<arr.length; i++){
+            arr[i] = arr[i]*10;
+        }
+        return arr ;
+    }
+
 }
