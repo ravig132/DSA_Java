e703b8ad111a663a0a4b31d8a80cec5d00ab6c9e
Ravi <gangwarr132@gmail.com>
2026-09-02 15:25:00 +0530

    shift element by k position modified and can be handle any k digit

--- shiftElementByKPositionArray.java
+++ shiftElementByKPositionArray.java
@@ -5,9 +5,9 @@ public class shiftElementByKPositionArray {
         int [] arr = {10,20,30,40,50,60,70,80,90};
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of position to shift array : ");
-        int position = input.nextInt();
+        int k = input.nextInt();
 
-        int[] newArr = shiftElementByKPosition(arr,position);
+        int[] newArr = shiftElementByKPosition(arr,k);
 
         for (int i : newArr) {
             System.out.print(i+" ");
@@ -18,7 +18,9 @@ public class shiftElementByKPositionArray {
 
 
     }
-    static int[] shiftElementByKPosition(int[] arr, int position){
+    static int[] shiftElementByKPosition(int[] arr, int k){
+
+        int position = k%arr.length ;
         int [] temp = new int[position];
         int last = arr.length-1;
 
@@ -27,11 +29,10 @@ public class shiftElementByKPositionArray {
             last-- ;
         }
 
-        for (int i=last; i > position-1 ; i--) {
-
-            arr[i] = arr[i-3];
-
+        for (int lastIndex = arr.length-1 ; lastIndex > position-1 ; lastIndex-- ){
+            arr[lastIndex] = arr[lastIndex-position];
         }
+
 
         int tempLast = temp.length-1 ;
         for (int i = 0; i < position; i++) {
