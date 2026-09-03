170d62864f1f665d3ceca56322c9747828d8814b
Ravi <gangwarr132@gmail.com>
2026-09-03 14:29:01 +0530

    missing number in an array (0-range) solved

--- missingNumberArray.java
+++ missingNumberArray.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class missingNumberArray {
+    public static void main(String[] args) {
+
+        int [] arr ={0,2,4,1,3};
+        System.out.println(missingNum(arr));
+
+    }
+
+    static int missingNum(int [] arr){
+        int xorSum = 0 ;
+
+        for (int i = 0; i < arr.length; i++) {
+            xorSum ^= arr[i];
+            xorSum ^= i+1 ;
+        }
+        return xorSum ;
+    }
 }
